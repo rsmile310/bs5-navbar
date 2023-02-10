ae67c336dfd25d61727d954c0e909ff46d8c627c
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2023-02-11 03:07:18 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let navLinkEle = document
+  .querySelector(".bd-navbar-nav")
+  .querySelectorAll(".nav-link");
+let subNavLinkEle = document.querySelectorAll(".sub-nav-link");
+
+navLinkEle.forEach(function (item) {
+  item.addEventListener("click", function () {
+    navLinkEle.forEach(function (e) {
+      if (e.classList.contains("active")) e.classList.remove("active");
+    });
+    if (!item.classList.contains("active")) item.classList.add("active");
+  });
+});
+
+subNavLinkEle.forEach(function (item) {
+  item.addEventListener("click", function () {
+    subNavLinkEle.forEach(function (e) {
+      if (e.classList.contains("active")) e.classList.remove("active");
+    });
+    if (!item.classList.contains("active")) item.classList.add("active");
+  });
+});
